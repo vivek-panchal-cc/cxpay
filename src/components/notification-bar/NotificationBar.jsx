16f83bedf398b12117693df462fb20974928a5ac
Nirbhay Dubey <Nirbhay.d@cctdigisol.local>
2023-04-05 13:56:40 +0530

    commit 0.1.0 added changes for fixed header of profile nad notification 05-April-2023

--- src/components/notification-bar/NotificationBar.jsx
+++ src/components/notification-bar/NotificationBar.jsx
@@ -6,23 +6,23 @@ const NotificationBar = (props) => {
   const { issearchbarvisible = false } = props;
   const navbarRef = useRef(null);
 
-  // useEffect(() => {
-  //   if (!navbarRef.current) return;
-  //   const body = document.querySelector("body");
-  //   const bar = navbarRef.current;
-  //   const handleScroll = (event) => {
-  //     const scrollTop = event.target.scrollTop;
-  //     const elmBottom = bar.offsetTop + bar.clientHeight;
-  //     if (scrollTop > elmBottom) body.classList.add("sticky_appear");
-  //     else body.classList.remove("sticky_appear");
-  //   };
-  //   document.querySelector("body").addEventListener("scroll", handleScroll);
-  //   return () => {
-  //     document
-  //       .querySelector("body")
-  //       .removeEventListener("scroll", handleScroll);
-  //   };
-  // }, [navbarRef]);
+  useEffect(() => {
+    if (!navbarRef.current) return;
+    const handleScroll = (event) => {
+      const body = event.target;
+      const bar = navbarRef.current;
+      const scrollTop = event.target.scrollTop;
+      const elmBottom = bar.offsetTop + bar.clientHeight;
+      if (scrollTop > 10) body.classList.add("sticky_appear");
+      else body.classList.remove("sticky_appear");
+    };
+    document.querySelector("body").addEventListener("scroll", handleScroll);
+    return () => {
+      document
+        .querySelector("body")
+        .removeEventListener("scroll", handleScroll);
+    };
+  }, [navbarRef]);
 
   return (
     <div className={`dashboard-top-sec no-search-ontop`} ref={navbarRef}>
