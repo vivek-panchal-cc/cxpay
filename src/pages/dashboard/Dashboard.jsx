a99e2876f53fcadafa674bb444b31b4675c11b47
Nirbhaydubey <nirbhaydube4@gmail.com>
2023-07-10 13:00:36 +0530

    commit 0.1.0 recent-activity slice 10-July-2023

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -168,7 +168,7 @@ const Dashboard = () => {
             {/* Recent Activity */}
             <RecentActivities
               loading={loadingAct}
-              activitiesList={activitiesList ? activitiesList.slice(0, 10) : []}
+              activitiesList={activitiesList ? activitiesList.slice(0, 5) : []}
             />
           </div>
           {/*   <!-- Dashboard card section starts --> */}
