6a131c0dfe318c6ec25fb7e707da180c2ca57f88
CCT Macmini <>
2024-04-12 16:30:23 +0530

    Instead of showing modal for send mail directly we hit the send mail by clicking on send mail on header.

--- src/components/notification-bar/NotificationBar.jsx
+++ src/components/notification-bar/NotificationBar.jsx
@@ -1,20 +1,24 @@
+import { LoaderContext } from "context/loaderContext";
 import { LoginContext } from "context/loginContext";
 import { SystemOptionsContext } from "context/systemOptionsContext";
+import { apiRequest } from "helpers/apiRequests";
 import React, { useContext, useEffect, useRef } from "react";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { IconAlert, IconArrowRight } from "styles/svgs";
 import KycChecked from "./kyc-checked/KycChecked";
+import { toast } from "react-toastify";
 import NotificationDropdown from "./notification-dropdown/NotificationDropdown";
 import ProfileDropdown from "./profile-dropdown/ProfileDropdown";
 
 const NotificationBar = (props) => {
   const navbarRef = useRef(null);
   const navigate = useNavigate();
+  const { setIsLoading } = useContext(LoaderContext);
   const { profile } = useSelector((state) => state.userProfile);
   const { user_type = "" } = profile || {};
   const { MANUAL_KYC } = useContext(SystemOptionsContext);
-  const { loginCreds } = useContext(LoginContext);
+  const { loginCreds, setLoginCreds } = useContext(LoginContext);
   const { kyc_message, show_renew_button, show_renew_section } = loginCreds;
   const kycType = MANUAL_KYC?.toString() === "true" ? "MANUAL" : "SYSTEM";
 
@@ -43,6 +47,36 @@ const NotificationBar = (props) => {
       )}`;
     } catch (error) {
       if (typeof error === "string") return toast.error(error);
+    }
+  };
+
+  const sendMail = async () => {
+    setIsLoading(true);
+    try {
+      const reqParams = {
+        is_apply_for_renew: "true",
+        kyc_type: kycType,
+        device_type: "web",
+        send_email: true,
+        kyc_status: "false",
+      };
+      const { data } = await apiRequest.updateCustomerKyc(reqParams);
+      if (!data.success) throw data.message;
+      toast.success(`${data.message}`);
+      setLoginCreds((ls) => ({
+        ...ls,
+        renew_kyc_approved_status:
+          data.data.kyc_renew_data?.renew_kyc_approved_status || "",
+        renew_kyc_attempt_count:
+          data.data.kyc_renew_data?.renew_kyc_attempt_count || "",
+        show_renew_section: data.data.kyc_renew_data?.show_renew_section || "",
+        show_renew_button: Boolean(data.data.kyc_renew_data?.show_renew_button),
+        kyc_message: data.data.kyc_renew_data?.kyc_message || "",
+      }));      
+    } catch (error) {
+      if (typeof error === "string") return toast.error(error);
+    } finally {
+      setIsLoading(false);
     }
   };
 
@@ -77,7 +111,7 @@ const NotificationBar = (props) => {
             ) : (
               (show_renew_section === "renew_limit_exceed" ||
                 show_renew_section === "renew_limit_exceed_and_disable") && (
-                <a className="kyc-expire-link" onClick={redirectMail}>
+                <a className="kyc-expire-link" onClick={sendMail}>
                   <span>Send Mail</span>
                 </a>
               )
