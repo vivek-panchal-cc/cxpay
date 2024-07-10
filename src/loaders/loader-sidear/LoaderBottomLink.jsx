366d4b15ef409abd41454b3bfd225f42735bb13d
CCT Macmini <vivek.p@communicationcrafts.com>
2024-07-10 14:26:05 +0530

    Left side bar loader modified.

--- src/loaders/loader-sidear/LoaderBottomLink.jsx
+++ src/loaders/loader-sidear/LoaderBottomLink.jsx
@@ -8,9 +8,9 @@ const LoaderBottomLink = () => (
     backgroundColor="#0978f6"
     foregroundColor="#2a81c5"
   >
-    <rect x="20" y="20" rx="10" ry="10" width="100%" height="45" />
-    <rect x="20" y="75" rx="10" ry="10" width="100%" height="45" />
-    <rect x="20" y="130" rx="10" ry="10" width="100%" height="45" />
+    <rect x="0" y="20" rx="10" ry="10" width="110%" height="45" />
+    <rect x="0" y="75" rx="10" ry="10" width="110%" height="45" />
+    <rect x="0" y="130" rx="10" ry="10" width="110%" height="45" />
   </ContentLoader>
 );
 
--- src/loaders/loader-sidear/LoaderMainLink.jsx
+++ src/loaders/loader-sidear/LoaderMainLink.jsx
@@ -8,12 +8,12 @@ const LoaderMainLink = () => (
     backgroundColor="#0978f6"
     foregroundColor="#2a81c5"
   >
-    <rect x="20" y="20" rx="10" ry="10" width="100%" height="45" />
-    <rect x="20" y="75" rx="10" ry="10" width="100%" height="45" />
-    <rect x="20" y="130" rx="10" ry="10" width="100%" height="45" />
-    <rect x="20" y="185" rx="10" ry="10" width="100%" height="45" />
-    <rect x="20" y="240" rx="10" ry="10" width="100%" height="45" />
-    <rect x="20" y="295" rx="10" ry="10" width="100%" height="45" />
+    <rect x="0" y="20" rx="10" ry="10" width="110%" height="45" />
+    <rect x="0" y="75" rx="10" ry="10" width="110%" height="45" />
+    <rect x="0" y="130" rx="10" ry="10" width="110%" height="45" />
+    <rect x="0" y="185" rx="10" ry="10" width="110%" height="45" />
+    <rect x="0" y="240" rx="10" ry="10" width="110%" height="45" />
+    <rect x="0" y="295" rx="10" ry="10" width="110%" height="45" />
   </ContentLoader>
 );
 
