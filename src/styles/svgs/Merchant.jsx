e8561d55307ca97e44f3aee0c6b760a40c548af7
Vivek Panchal <vivek.p@communicationcrafts.com>
2024-12-03 18:13:10 +0530

    Merchant Icon height width change.

--- src/styles/svgs/Merchant.jsx
+++ src/styles/svgs/Merchant.jsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 const Merchant = (props) => (
   <svg
-    width={24}
-    height={24}
+    width={18}
+    height={19}
     viewBox="0 0 24 24"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
