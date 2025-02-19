94f6441124331aa2e950c5f98d94703dce165896
Vivek Panchal <vivek.p@communicationcrafts.com>
2025-02-19 10:48:45 +0530

    Bugfix for Add Contacts and Custome QR Specification

--- src/schemas/validationSchema.js
+++ src/schemas/validationSchema.js
@@ -208,7 +208,8 @@ const setQrAmount = yup.object().shape({
   specification: yup
     .string()
     .matches(exp0ContainOnlySpace, "Space is not allowed")
-    .matches(/^[^,]*$/, "Commas are not allowed")
+    // .matches(/^[^,]*$/, "Commas are not allowed")
+    .matches(/^[a-zA-Z0-9 -]*$/, "Special characters are not allowed")
     .max(50, "Maximum limit is 50 characters.")
     .required("Please enter specifications"),
 });
