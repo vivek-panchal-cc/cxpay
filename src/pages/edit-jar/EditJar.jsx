9e1737ad720f5f6d286d77604225a98910c96447
Vivek Panchal <vivek.p@communicationcrafts.com>
2025-05-23 10:55:56 +0530

    Validation updated for EditJar icons.

--- src/pages/edit-jar/EditJar.jsx
+++ src/pages/edit-jar/EditJar.jsx
@@ -129,8 +129,15 @@ const EditJar = () => {
 
   useEffect(() => {
     setCategoryParentId(formik.values.jar_parent_category_id);
-    formik.setFieldValue("jar_icon", null);
-  }, [formik.values.jar_parent_category_id]);
+
+    // Get new list of icons for the selected category
+    const newIcons = jarIcon.map((icon) => icon.id);
+    const currentIcon = formik.values.jar_icon;
+
+    if (!newIcons.includes(currentIcon)) {
+      formik.setFieldValue("jar_icon", null);
+    }
+  }, [formik.values.jar_parent_category_id, jarIcon]);
 
   // For making input scroll into view on validation error
   useEffect(() => {
