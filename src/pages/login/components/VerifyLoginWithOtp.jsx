4273a07691a51cd969c249a3b94989e52dde1bf7
Vivek Panchal <vivek.p@communicationcrafts.com>
2025-07-30 13:05:25 +0530

    Label dynamically changed.

--- src/pages/login/components/VerifyLoginWithOtp.jsx
+++ src/pages/login/components/VerifyLoginWithOtp.jsx
@@ -118,7 +118,9 @@ function VerifyLoginWithOtp(props) {
           </div>
         </div>
         <div className="modal-body">
-          <h3>Verify your Phone Number</h3>
+          <h3>
+            Verify your {loginType === "email" ? "Email" : "Phone Number"}
+          </h3>
           <p>Please enter confirmation code</p>
           <form className="login-otp-numbers" onSubmit={formik.handleSubmit}>
             <div className="form-field">
